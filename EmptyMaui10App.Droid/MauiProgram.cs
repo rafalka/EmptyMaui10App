d263b81b5d46296c7bd1ca53ffd026271824d02f
Rafał Kukla <rkukla@thinktime.com>
2026-06-15 10:23:51 +0200

    Add project files.

---
+++ EmptyMaui10App.Droid/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace EmptyMaui10App.Droid
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ EmptyMaui10App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EmptyMaui10App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
